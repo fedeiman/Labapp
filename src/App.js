d456ecbc1d3939cb79932f5186777a849bfa42a9
matiasiman <matiasiman@gmail.com>
2020-12-21 23:05:17 -0300

    Added and working pouchDB

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import Login from './components/login';
+const PouchDB = require('pouchdb-browser');
+const pouchDB = PouchDB.default.defaults();
 
+const db = new pouchDB('MyDB');
+
+var doc = {
+  "_id": "mittens",
+  "name": "Mittens",
+  "occupation": "kitten",
+  "age": 3,
+  "hobbies": [
+    "playing with balls of yarn",
+    "chasing laser pointers",
+    "lookin' hella cute"
+  ]
+};
+
+db.put(doc);
 function App() {
+  db.get('mittens').then(function (doc) {
+    console.log(doc);
+  });
   return (
     <div>
       <Login/>
